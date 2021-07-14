a204daca75d8d0902bb49b1a077662e7eced172f
Yoshiki Sakamoto <y.sakamoto@j-ij.com>
2021-07-14 17:21:52 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -48,7 +48,7 @@ class CMakeBuild(build_ext):
         cmake_args = [
             "-DCMAKE_LIBRARY_OUTPUT_DIRECTORY={}".format(extdir),
             "-DPYTHON_EXECUTABLE={}".format(sys.executable),
-            "-DEXAMPLE_VERSION_INFO={}".format(self.distribution.get_version()),
+            "-DPYQUBO_VERSION_INFO={}".format(self.distribution.get_version()),
             "-DCMAKE_BUILD_TYPE={}".format(cfg),  # not used on MSVC, but no harm
         ]
         build_args = []
