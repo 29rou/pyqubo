15728d3eaed745af85cd81e683b22231416803a9
Yoshiki Sakamoto <y.sakamoto@j-ij.com>
2021-07-14 16:28:02 +0900

    Create package_info.py

---
+++ package_info.py
@@ -0,0 +1,8 @@
+class PackageInfo(object):
+    def __init__(self, info_file):
+        with open(info_file) as f:
+            exec(f.read(), self.__dict__)
+        self.__dict__.pop('__builtins__', None)
+
+    def __getattribute__(self, name):
+        return super(PackageInfo, self).__getattribute__(name)
