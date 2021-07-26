49ab2da0c3df32bd659ac2e8d2d6ad8ddcaa9af6
Yoshiki Sakamoto <y.sakamoto@j-ij.com>
2021-07-26 09:42:29 +0900

    Update pybind11.cmake

--- external/pybind11.cmake
+++ external/pybind11.cmake
@@ -6,5 +6,6 @@ FetchContent_Declare(
     GIT_REPOSITORY  https://github.com/pybind/pybind11
     GIT_TAG         v2.6.2
 )
-
+ser(EIGEN_MPL2_ONLY ON)
+set(EIGEN_CPP_STANDARD -std=c++11)
 FetchContent_MakeAvailable(pybind11)
